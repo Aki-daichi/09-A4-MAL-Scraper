c77b6b420e8a3df7bd9efa5914fa910da289fff7
Dzaka Fadhlillah Hakim <dzaka04@gmail.com>
2024-04-19 10:34:17 +0700

    Zaki as base
    credit to Zaki

--- manga/manga/spiders/MangaScraper.py
+++ manga/manga/spiders/MangaScraper.py
@@ -1,25 +1,52 @@
-# Imports
 import scrapy
+import json
+import os
 
-# Main Scrapper
-class MangaSpider(scrapy.Spider) :
+class MangaSpider(scrapy.Spider):
     name = "manga-spider"
     start_urls = ["https://myanimelist.net/topmanga.php"]
-    num_titles = 0
+    for i in range(1, 5):
+        start_urls.append("https://myanimelist.net/topmanga.php?limit=" + str(i * 50))
+    i = 0
 
     def parse(self, response):
         # Mengekstrak semua elemen tr dengan kelas 'ranking-list'
-        for manga in response.css('tr.ranking-list'):
-            # Menghasilkan item dalam format JSON langsung di yield
-            yield {
-                'rank': manga.css('span[class^="lightLink top-anime-rank-text rank"]::text').get(),
-                'title': manga.css('a.hoverinfo_trigger.fs14.fw-b::text').get()
-            }
+        for mangas in response.css('tr.ranking-list'):
+            # Mengambil data-data yang diperlukan
+            rank = mangas.css("td.rank.ac span.lightLink::text").get()
+            volume = mangas.css("div.information.di-ib.mt4::text").getall()[0].replace('\n', ' ').strip()
+            waktupublish = mangas.css("div.information.di-ib.mt4::text").getall()[1].replace('\n', ' ').strip()
+            title = mangas.css('a.hoverinfo_trigger.fs14.fw-b::text').get()
+            links = mangas.css("a.hoverinfo_trigger::attr(href)").get()
 
-            next_page = response.css("a.link-blue-box.next::attr(href)").get()
-            if (next_page is not None) and (self.num_titles <= 200):
-                self.num_titles += 1
-                next_page_url = "https://myanimelist.net/topmanga.php"+next_page
-                yield response.follow(next_page_url, callback = self.parse)
+            # Membuat request baru untuk setiap manga
+            request = scrapy.Request(response.urljoin(links), callback=self.parse_creator)
+            request.meta['rank'] = rank
+            request.meta['volume'] = volume
+            request.meta['waktupublish'] = waktupublish
+            request.meta['title'] = title
+            yield request
+
+    def parse_creator(self, response):
+        # Memasukkan data kedalam variable
+        rank = response.meta['rank']
+        title = response.meta['title']
+        volume = response.meta['volume']
+        waktupublish = response.meta['waktupublish']
+
+        # Menghasilkan item dalam format JSON langsung di yield
+        yield {
+            'rank': rank,
+            'score': response.css("div.score-label::text").get(),
+            'popularity': response.css("span.numbers.popularity strong::text").get().strip('#'),
+            'title': title,
+            'volume': volume,
+            'waktupublish': waktupublish,
+            'synopsis': (response.css('span[itemprop="description"]::text').getall()[0] +
+                         response.css('span[itemprop="description"]::text').getall()[2]).replace("\r\n", "<br><br>"),
+            'imglink': response.css("div#myanimelist div.wrapper div#contentWrapper div#content table tr "
+                                    "td.borderClass div.leftside div a img::attr(data-src)").get(),
+            'author': response.css("span.information.studio.author a::text").get()
+        }
 
 '''scrapy crawl manga-spider -o ..\view\hasil.json'''
